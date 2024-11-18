5aee44e9061229d82c13c25e2dc6fddb41f0e113
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2024-11-18 14:26:25 -0500

    content rating 2

---
+++ 01 REACT/00 React-Intro/content_rating/src/Components/ContentRating.jsx
@@ -0,0 +1,43 @@
+
+import React, { Component } from 'react';
+import './ContentRating.css';
+
+class ContentRating extends Component {
+  constructor() {
+    super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+    }
+  }
+}
+  render() {
+    return (
+      <div className='content-rating'>
+      <p>
+      Thing to like or dislike
+      </p>
+      <div className='rating-buttons'>
+      <button className="like-button" onClick={this.state.handleLike}>
+          Like ({this.state.likes})
+        </button>
+        <button className="dislike-button" onClick={this.state.handleDislike}>
+          Dislike ({this.state.dislikes})
+        </button>
+      </div>
+      <p>Total Ratings: ({this.state.likes+this.state.dislikes})</p>
+  </div>
+    );
+  }
+}
+
+export default ContentRating;
