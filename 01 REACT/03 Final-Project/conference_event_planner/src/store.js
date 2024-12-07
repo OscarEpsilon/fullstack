9f48f21e485f6ab07f5a4a1fcfffcae095923635
OscarEpsilon <wv@ranchero.com>
2024-12-07 16:00:57 -0500

    ignore, wrong computer

---
+++ 01 REACT/03 Final-Project/conference_event_planner/src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+export default configureStore({
+  reducer: {
+    venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
+  },
+});
